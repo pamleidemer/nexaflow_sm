509ec80a51bdee552845b8834593768ef41b1a1c
pamleidemer <pamelaleidemer@gmail.com>
2025-06-10 16:03:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NexaFlow Sistema Moveleiro</h1>
+      <p>Sistema rodando localmente.</p>
+    </div>
+  );
+}
+
+export default App;
